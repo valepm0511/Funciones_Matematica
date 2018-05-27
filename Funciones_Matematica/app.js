7b5d0d6fa48889e6020845044b63c811c18270cf
valeria <valeria.penaloza5@gmail.com>
2018-05-26 22:48:47 -0400

    ejercicio funciones sumar y multiplicar

---
+++ Funciones_Matematica/app.js
@@ -0,0 +1,9 @@
+const add = function(num1,num2){
+	return num1 + num2;
+}
+const multiply = function(num1,num2){
+	return num1 * num2;
+}
+const resultSum = add(9824,777);
+const resultMultiply = multiply(36325,resultSum);
+document.write("Resultado: " + resultMultiply);
